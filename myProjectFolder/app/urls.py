ae37143743d37d5e1a34c8cf2be15ca63850b844
DevanshiMallick <devanshimallick0804@gmail.com>
2024-12-19 00:00:31 +0530

    introducedd the concept of categories and sort by option.

--- myProjectFolder/app/urls.py
+++ myProjectFolder/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views #. means from this partocular folder look for "views" file
 
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('home/', views.index, name= "home"),
+    path('products/', views.product_list, name='product_list'),
 ]
--- myProjectFolder/app/views.py
+++ myProjectFolder/app/views.py
@@ -3,17 +3,37 @@ from django.http import HttpResponse
 
 # Create your views here.
 #creating the first landing page
-def index(request):
-    code= """
-    <h1>hello</h1>
-    """
-    #return HttpResponse(code)
+# def index(request):
+#     code= """
+#     <h1>hello</h1>
+#     """
+#     #return HttpResponse(code)
 
-    #returning the html page for the app
-    html_page= "app/index.html"
-    return render(request, html_app)
+#     #returning the html page for the app
+#     html_page= "app/index.html"
+#     return render(request, html_app)
 
 
-def index(request):
-    #redirect("home")
-    return HttpResponse("hello world")
+# def index(request):
+#     #redirect("home")
+#     return HttpResponse("hello world")
+
+
+# app/views.py
+from django.shortcuts import render
+
+def home(request):
+    categories = [
+        "Kurtas & Suits",
+        "Kurtis, Tunics & Tops",
+        "Sarees",
+        "Ethnic Wear",
+        "Dresses",
+        "Tops",
+        "Tshirts",
+        "Jeans",
+    ]
+    
+    return render(request, 'homepage/home.html', {'categories': categories})
+
+
